4de20560bf76d3755a0191222cd674f6fe6386b1
weihang <pehweihang@live.com>
2022-01-14 05:09:56 +0800

    changed telescope grep keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -67,5 +67,5 @@ keymap("v", "p", '"_dP', opts)
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>t", "<cmd>Telescope live_grep<cr>", opts)
 
