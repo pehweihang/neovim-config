11f54c8ada5b7e805ab8b5bf811c5c5c276193a6
weihang <pehweihang@live.com>
2022-01-15 02:43:37 +0800

    changed colorscheme to gruvbox-material

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,7 +6,9 @@
 --   set background=dark
 -- endtry
 -- ]]
-local colorscheme = "gruvbox"
+local colorscheme = "gruvbox-material"
+vim.api.nvim_set_var('gruvbox_material_palette', 'material')
+vim.api.nvim_set_var('gruvbox_material_background', 'medium')
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,7 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use "ellisonleao/gruvbox.nvim"
+  use "sainnhe/gruvbox-material"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
