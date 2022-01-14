077d9cbd06e5e60c7bb139aeadd42b6dc0320778
weihang <pehweihang@live.com>
2022-01-15 02:04:00 +0800

    added alpha, impatient, indentline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -53,6 +53,10 @@ return packer.startup(function(use)
   use 'nvim-lualine/lualine.nvim'
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
+  use "lewis6991/impatient.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
+  use "goolord/alpha-nvim"
+  use "antoinemadec/FixCursorHold.nvim"
 
   -- Colorschemes
   use "ellisonleao/gruvbox.nvim"
