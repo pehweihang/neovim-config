03bb850443695815dff0b2482e9216f23bbfe160
pehweihang <pehweihang@live.com>
2022-01-18 22:30:26 +0800

    added r language server

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,8 +1,9 @@
-local status_ok, _ = pcall(require, "lspconfig")
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
   return
 end
 
+lspconfig.r_language_server.setup{ on_attach = require('user.lsp.handlers').on_attach}
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,15 +8,17 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
+local sources = {
     formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
     formatting.black.with { extra_args = { "--fast" } },
     -- formatting.yapf,
-    formatting.stylua,
     diagnostics.flake8,
-  },
+}
+
+null_ls.setup {
+  debug = false,
+  sources = sources,
+
   -- format on save
   on_attach = function(client)
       if client.resolved_capabilities.document_formatting then
