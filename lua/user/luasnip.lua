9d2f808f806d1ccebc5c3be013e3f163b72d863c
weihang <pehweihang@live.com>
2022-01-31 01:16:12 +0800

    added snippets for cp

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -3,13 +3,6 @@ if not status_ok then
 	return
 end
 
-luasnip.snippets = {all = {}, html = {}}
-
--- enable html snippets in javascript/javascript(REACT)
-luasnip.snippets.javascript = luasnip.snippets.html
-luasnip.snippets.javascriptreact = luasnip.snippets.html
-luasnip.snippets.typescriptreact = luasnip.snippets.html
-require("luasnip/loaders/from_vscode").load({include = {"html"}})
-
-
-require("luasnip/loaders/from_vscode").load()
+require("luasnip/loaders/from_vscode").load{
+  paths = "~/.config/nvim/lua/user/cp-snippets/"
+}
