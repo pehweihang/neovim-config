0744d67f0562625523eb295ec6cf23562db7c1b2
pehweihang <pehweihang@live.com>
2022-03-05 18:49:07 +0800

    added filetype, lightspeed neoclip

--- init.lua
+++ init.lua
@@ -1,22 +1,25 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.autopairs"
-require "user.comment"
-require "user.treesitter"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentLine"
-require "user.alpha"
-require "user.bufferline"
-require "user.luasnip"
-require "user.trouble"
-require "user.lsp_signature"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.colorscheme")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.autopairs")
+require("user.comment")
+require("user.treesitter")
+require("user.gitsigns")
+require("user.nvim-tree")
+require("user.lualine")
+require("user.toggleterm")
+require("user.project")
+require("user.impatient")
+require("user.indentLine")
+require("user.alpha")
+require("user.bufferline")
+require("user.luasnip")
+require("user.trouble")
+require("user.lsp_signature")
+require("user.nvim-neoclip")
+require("user.lightspeed")
+require("user.filetype")
